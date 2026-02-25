07eb1f245ef4fa20364a86129515b758dbbf1e45
Maicondemetrio <maiconcaco69@gmail.com>
2026-02-24 23:38:44 -0300

    Creating asset folder Inserting all asset Creating the menu: background, sound, and, fonts Creating the const file

--- Code/menu.py
+++ Code/menu.py
@@ -16,18 +16,19 @@ def get_rect(center):
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/Free-Mountain.webp')
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self, MENU_OPITION=None):
+    def run(self):
 
-        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.load('./asset/game-loop-5.mp3')
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
-            for i in range(len(MENU_OPITION)):
+            for i in range(len(MENU_OPTION)):
                 self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()
